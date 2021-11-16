61584675885c182ab69527787572c9a546bf86fe
Artem134134 <car322132@gmail.com>
2021-11-16 16:09:17 +0300

    Remove hardcoded barmens list

--- app.rb
+++ app.rb
@@ -23,6 +23,11 @@ def get_db
   	return db
 end
 
+before do
+	db = get_db
+	@barmens = db.execute 'select * from Barmens'
+end
+
  configure do
  	db = get_db	
 	db.execute 'CREATE TABLE IF NOT EXISTS
